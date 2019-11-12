dd436e2d042da2b97bba58c68b57035076ad9cdd
Faschal <faschal21@gmail.com>
2019-11-13 00:38:16 +0700

    comment email

--- contacts/views.py
+++ contacts/views.py
@@ -27,13 +27,13 @@ def contact(request):
     contact.save()
 
     # Send Email
-    send_mail(
-      'Property Listing Inquiry',
-      'There has been an inquiry for '+ listing + '. Sign into the admin panel for more info',
-      'faschal25@gmail.com',
-      [realtor_email, 'faschalaja@gmail.com'],
-      fail_silently=False
-    )
+    # send_mail(
+    #   'Property Listing Inquiry',
+    #   'There has been an inquiry for '+ listing + '. Sign into the admin panel for more info',
+    #   'faschal25@gmail.com',
+    #   [realtor_email, 'faschalaja@gmail.com'],
+    #   fail_silently=False
+    # )
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
     return redirect('/listings/'+listing_id)
